37b75a4ff6d7feabd13d6f28f30c6e3642728db4
JauffretOscar <oscar.jauffret@ulb.be>
2025-03-09 18:03:11 +0100

    Commented dataAnalysis.cpp

--- src/dataAnalysis.cpp
+++ src/dataAnalysis.cpp
@@ -9,7 +9,11 @@
 
 using namespace std;
 
-
+/**
+ * @brief Check if the processing times of a file are below 255. This function was used to check if we could use uint8_t to store the processing times
+ * @param filename Path to the file
+ * @return True if all the processing times are below 255, false otherwise
+ */
 bool checkProcessingTimes(const string& filename) {
     ifstream file(filename);
     if (!file.is_open()) {
@@ -20,6 +24,7 @@ bool checkProcessingTimes(const string& filename) {
     int numJobs, numMachines;
     file >> numJobs >> numMachines;     // Read the number of jobs and machines
 
+    // Read all the processing times and check if they are below 255
     for (int i = 0; i < numJobs; ++i) {
         for (int j = 0; j < numMachines; ++j) {
             int machineNumber, processingTime;
@@ -35,6 +40,11 @@ bool checkProcessingTimes(const string& filename) {
     return true;
 }
 
+/**
+ * @brief Check if the processing times of all the files in a directory are below 255
+ * @param directory Path to the directory
+ * @return True if all the processing times are below 255, false otherwise
+ */
 bool checkAllProcessingTimes(const string& directory) {
     for (const auto& entry : filesystem::directory_iterator(directory)) {
         if (entry.is_regular_file() && entry.path().extension() == ".txt") {
--- src/dataAnalysis.hpp
+++ src/dataAnalysis.hpp
@@ -10,7 +10,18 @@
 
 using std::string;
 
+/**
+ * @brief Check if the processing times of a file are below 255. This function was used to check if we could use uint8_t to store the processing times
+ * @param filename Path to the file
+ * @return True if all the processing times are below 255, false otherwise
+ */
 bool checkProcessingTimes(const string& filename);
+
+/**
+ * @brief Check if the processing times of all the files in a directory are below 255
+ * @param directory Path to the directory
+ * @return True if all the processing times are below 255, false otherwise
+ */
 bool checkAllProcessingTimes(const string& directory);
 
 #endif //FLOWSHOPSLS_DATAANALYSIS_HPP
