dcca33bdc26d264469c2f60aa3364450b57bde5c
JauffretOscar <oscar.jauffret@ulb.be>
2025-04-08 14:31:42 +0200

    stringify comments

--- include/utils/stringify.hpp
+++ include/utils/stringify.hpp
@@ -10,7 +10,16 @@
 
 using std::string;
 
+/**
+ * @namespace stringify
+ * @brief The stringify namespace contains functions to convert enumeration types to strings.
+ */
 namespace stringify {
+    /**
+     * @brief Convert the PivotingRule enumeration to a string.
+     * @param p The PivotingRule enumeration value.
+     * @return The string representation of the PivotingRule enumeration value.
+     */
     inline string pivot(PivotingRule p) {
         switch (p) {
             case PivotingRule::FIRST_IMPROVEMENT: return "first";
@@ -20,6 +29,11 @@ namespace stringify {
         return "unknown";
     }
 
+    /**
+     * @brief Convert the NeighbourhoodStructure enumeration to a string.
+     * @param n The NeighbourhoodStructure enumeration value.
+     * @return The string representation of the NeighbourhoodStructure enumeration value.
+     */
     inline string neighbourhood(NeighbourhoodStructure n) {
         switch (n) {
             case NeighbourhoodStructure::TRANSPOSE: return "transpose";
@@ -30,6 +44,11 @@ namespace stringify {
         return "unknown";
     }
 
+    /**
+     * @brief Convert the InitializationMethod enumeration to a string.
+     * @param i The InitializationMethod enumeration value.
+     * @return The string representation of the InitializationMethod enumeration value.
+     */
     inline string init(InitializationMethod i) {
         switch (i) {
             case InitializationMethod::RANDOM: return "random";
@@ -39,6 +58,11 @@ namespace stringify {
         return "unknown";
     }
 
+    /**
+     * @brief Convert the AlgorithmType enumeration to a string.
+     * @param v The AlgorithmType enumeration value.
+     * @return The string representation of the AlgorithmType enumeration value.
+     */
     inline string vnd(VNDStrategy v) {
         switch (v) {
             case VNDStrategy::TEI: return "tei";
